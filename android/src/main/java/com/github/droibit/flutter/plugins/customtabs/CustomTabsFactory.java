f6a436596f4435ca725fa9748b2c61cb81530eab
Shinya Kumagai <roomful.rooms@gmail.com>
2021-05-21 18:42:41 +0900

    Claenup CustomTabsPlugin for Android

---
+++ android/src/main/java/com/github/droibit/flutter/plugins/customtabs/CustomTabsFactory.java
@@ -0,0 +1,143 @@
+package com.github.droibit.flutter.plugins.customtabs;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.provider.Browser;
+
+import androidx.annotation.AnimRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.RestrictTo;
+import androidx.browser.customtabs.CustomTabsIntent;
+
+import com.droibit.android.customtabs.launcher.CustomTabsFallback;
+import com.droibit.android.customtabs.launcher.CustomTabsLauncher;
+
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+@SuppressWarnings({"ConstantConditions", "unchecked"})
+@RestrictTo(RestrictTo.Scope.LIBRARY)
+class CustomTabsFactory {
+    private static final String KEY_OPTIONS_TOOLBAR_COLOR = "toolbarColor";
+    private static final String KEY_OPTIONS_ENABLE_URL_BAR_HIDING = "enableUrlBarHiding";
+    private static final String KEY_OPTIONS_SHOW_PAGE_TITLE = "showPageTitle";
+    private static final String KEY_OPTIONS_DEFAULT_SHARE_MENU_ITEM = "enableDefaultShare";
+    private static final String KEY_OPTIONS_ENABLE_INSTANT_APPS = "enableInstantApps";
+    private static final String KEY_OPTIONS_ANIMATIONS = "animations";
+    private static final String KEY_HEADERS = "headers";
+    private static final String KEY_ANIMATION_START_ENTER = "startEnter";
+    private static final String KEY_ANIMATION_START_EXIT = "startExit";
+    private static final String KEY_ANIMATION_END_ENTER = "endEnter";
+    private static final String KEY_ANIMATION_END_EXIT = "endExit";
+    private static final String KEY_EXTRA_CUSTOM_TABS = "extraCustomTabs";
+
+    // Note: The full resource qualifier is "package:type/entry".
+    // https://developer.android.com/reference/android/content/res/Resources.html#getIdentifier(java.lang.String, java.lang.String, java.lang.String)
+    private static final Pattern animationIdentifierPattern = Pattern.compile("^.+:.+/");
+
+    private final Context context;
+
+    CustomTabsFactory(@NonNull Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    CustomTabsIntent createIntent(@NonNull Map<String, Object> options) {
+        final CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        if (options.containsKey(KEY_OPTIONS_TOOLBAR_COLOR)) {
+            final String colorString = (String) options.get(KEY_OPTIONS_TOOLBAR_COLOR);
+            builder.setToolbarColor(Color.parseColor(colorString));
+        }
+
+        if (options.containsKey(KEY_OPTIONS_ENABLE_URL_BAR_HIDING)
+                & ((Boolean) options.get(KEY_OPTIONS_ENABLE_URL_BAR_HIDING))) {
+            builder.enableUrlBarHiding();
+        }
+
+        if (options.containsKey(KEY_OPTIONS_DEFAULT_SHARE_MENU_ITEM) &&
+                ((Boolean) options.get(KEY_OPTIONS_DEFAULT_SHARE_MENU_ITEM))) {
+            builder.addDefaultShareMenuItem();
+        }
+
+        if (options.containsKey(KEY_OPTIONS_SHOW_PAGE_TITLE)) {
+            builder.setShowTitle(((Boolean) options.get(KEY_OPTIONS_SHOW_PAGE_TITLE)));
+        }
+
+        if (options.containsKey(KEY_OPTIONS_ENABLE_INSTANT_APPS)) {
+            builder.setInstantAppsEnabled(((Boolean) options.get(KEY_OPTIONS_ENABLE_INSTANT_APPS)));
+        }
+
+        if (options.containsKey(KEY_OPTIONS_ANIMATIONS)) {
+            applyAnimations(builder, ((Map<String, String>) options.get(KEY_OPTIONS_ANIMATIONS)));
+        }
+
+        final CustomTabsIntent customTabsIntent = builder.build();
+        onPostBuild(customTabsIntent.intent, options);
+        return customTabsIntent;
+    }
+
+    private void onPostBuild(@NonNull Intent intent, @NonNull Map<String, Object> options) {
+        if (options.containsKey(KEY_HEADERS)) {
+            Map<String, String> headers = (Map<String, String>) options.get(KEY_HEADERS);
+            Bundle bundleHeaders = new Bundle();
+            for (Map.Entry<String, String> header : headers.entrySet()) {
+                bundleHeaders.putString(header.getKey(), header.getValue());
+            }
+            intent.putExtra(Browser.EXTRA_HEADERS, bundleHeaders);
+        }
+    }
+
+    private void applyAnimations(@NonNull CustomTabsIntent.Builder builder,
+                                 @NonNull Map<String, String> animations) {
+        final int startEnterAnimationId =
+                animations.containsKey(KEY_ANIMATION_START_ENTER) ? resolveAnimationIdentifierIfNeeded(
+                        animations.get(KEY_ANIMATION_START_ENTER)) : -1;
+        final int startExitAnimationId =
+                animations.containsKey(KEY_ANIMATION_START_EXIT) ? resolveAnimationIdentifierIfNeeded(
+                        animations.get(KEY_ANIMATION_START_EXIT)) : -1;
+        final int endEnterAnimationId =
+                animations.containsKey(KEY_ANIMATION_END_ENTER) ? resolveAnimationIdentifierIfNeeded(
+                        animations.get(KEY_ANIMATION_END_ENTER)) : -1;
+        final int endExitAnimationId =
+                animations.containsKey(KEY_ANIMATION_END_EXIT) ? resolveAnimationIdentifierIfNeeded(
+                        animations.get(KEY_ANIMATION_END_EXIT)) : -1;
+
+        if (startEnterAnimationId != -1 && startExitAnimationId != -1) {
+            builder.setStartAnimations(context, startEnterAnimationId, startExitAnimationId);
+        }
+
+        if (endEnterAnimationId != -1 && endExitAnimationId != -1) {
+            builder.setExitAnimations(context, endEnterAnimationId, endExitAnimationId);
+        }
+    }
+
+    @AnimRes
+    private int resolveAnimationIdentifierIfNeeded(@NonNull String identifier) {
+        if (animationIdentifierPattern.matcher(identifier).find()) {
+            return context.getResources().getIdentifier(identifier, null, null);
+        } else {
+            return context.getResources().getIdentifier(identifier, "anim", context.getPackageName());
+        }
+    }
+
+    @NonNull
+    CustomTabsFallback createFallback(@NonNull Map<String, Object> options) {
+        final List<String> extraCustomTabs;
+        if (options.containsKey(KEY_EXTRA_CUSTOM_TABS)) {
+            extraCustomTabs = ((List<String>) options.get(KEY_EXTRA_CUSTOM_TABS));
+        } else {
+            extraCustomTabs = null;
+        }
+
+        final CustomTabsFallback fallback;
+        if (extraCustomTabs != null && !extraCustomTabs.isEmpty()) {
+            fallback = new CustomTabsLauncher.LaunchNonChromeCustomTabs(extraCustomTabs);
+        } else {
+            fallback = new CustomTabsLauncher.LaunchBrowser();
+        }
+        return fallback;
+    }
+}
--- android/src/main/java/com/github/droibit/flutter/plugins/customtabs/CustomTabsPlugin.java
+++ android/src/main/java/com/github/droibit/flutter/plugins/customtabs/CustomTabsPlugin.java
@@ -1,85 +1,101 @@
 package com.github.droibit.flutter.plugins.customtabs;
 
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
-import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
-import android.provider.Browser;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.browser.customtabs.CustomTabsIntent;
-import com.github.droibit.flutter.plugins.customtabs.internal.Launcher;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.PluginRegistry;
-import java.util.List;
+
+import com.droibit.android.customtabs.launcher.CustomTabsFallback;
+import com.droibit.android.customtabs.launcher.CustomTabsLauncher;
+
 import java.util.Map;
 
-public class CustomTabsPlugin implements MethodChannel.MethodCallHandler {
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 
-  /**
-   * Plugin registration.
-   */
-  public static void registerWith(PluginRegistry.Registrar registrar) {
-    final MethodChannel channel =
-        new MethodChannel(registrar.messenger(), "com.github.droibit.flutter.plugins.custom_tabs");
-    channel.setMethodCallHandler(new CustomTabsPlugin(registrar));
-  }
+public class CustomTabsPlugin implements FlutterPlugin, ActivityAware, MethodCallHandler {
 
-  private static final String KEY_OPTION = "option";
+    private static final String KEY_OPTION = "option";
 
-  private static final String KEY_URL = "url";
+    private static final String KEY_URL = "url";
 
-  private static final String KEY_EXTRA_CUSTOM_TABS = "extraCustomTabs";
+    private static final String CODE_LAUNCH_ERROR = "LAUNCH_ERROR";
 
-  private static final String CODE_LAUNCH_ERROR = "LAUNCH_ERROR";
+    @Nullable
+    private Activity activity;
 
-  private final PluginRegistry.Registrar registrar;
+    @Nullable
+    private MethodChannel channel;
 
-  private final Launcher launcher;
-
-  private CustomTabsPlugin(@NonNull PluginRegistry.Registrar registrar) {
-    this.registrar = registrar;
-    this.launcher = new Launcher(registrar.activeContext());
-  }
-
-  @SuppressWarnings("unchecked") @Override
-  public void onMethodCall(MethodCall call, final MethodChannel.Result result) {
-    switch (call.method) {
-      case "launch":
-        launch(((Map<String, Object>) call.arguments), result);
-        break;
-      default:
-        result.notImplemented();
-        break;
-    }
-  }
-
-  @SuppressWarnings("unchecked")
-  private void launch(@NonNull Map<String, Object> args, @NonNull MethodChannel.Result result) {
-    final Uri uri = Uri.parse(args.get(KEY_URL).toString());
-    final Map<String, Object> options = (Map<String, Object>) args.get(KEY_OPTION);
-    final CustomTabsIntent customTabsIntent = launcher.buildIntent(options);
-
-    final Context context;
-    if (registrar.activity() != null) {
-      context = registrar.activity();
-    } else {
-      context = registrar.context();
-      customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPlugin.FlutterPluginBinding binding) {
+        channel = new MethodChannel(binding.getBinaryMessenger(), "com.github.droibit.flutter.plugins.custom_tabs");
+        channel.setMethodCallHandler(this);
     }
 
-    try {
-      final List<String> extraCustomTabs;
-      if (options.containsKey(KEY_EXTRA_CUSTOM_TABS)) {
-        extraCustomTabs = ((List<String>) options.get(KEY_EXTRA_CUSTOM_TABS));
-      } else {
-        extraCustomTabs = null;
-      }
-      launcher.launch(context, uri, customTabsIntent, extraCustomTabs);
-      result.success(null);
-    } catch (ActivityNotFoundException e) {
-      result.error(CODE_LAUNCH_ERROR, e.getMessage(), null);
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPlugin.FlutterPluginBinding binding) {
+        if (channel == null) {
+            return;
+        }
+        channel.setMethodCallHandler(null);
     }
-  }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        onDetachedFromActivity();
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        onAttachedToActivity(binding);
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        activity = null;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull final MethodChannel.Result result) {
+        if ("launch".equals(call.method)) {
+            launch(((Map<String, Object>) call.arguments), result);
+        } else {
+            result.notImplemented();
+        }
+    }
+
+    @SuppressWarnings({"unchecked", "ConstantConditions"})
+    private void launch(@NonNull Map<String, Object> args, @NonNull MethodChannel.Result result) {
+        final Activity activity = this.activity;
+        if (activity == null) {
+            result.error(CODE_LAUNCH_ERROR, "Launching a CustomTabs requires a foreground activity.", null);
+            return;
+        }
+
+        final CustomTabsFactory factory = new CustomTabsFactory(activity);
+        try {
+            final Map<String, Object> options = (Map<String, Object>) args.get(KEY_OPTION);
+            final CustomTabsIntent customTabsIntent = factory.createIntent(options);
+            final Uri uri = Uri.parse(args.get(KEY_URL).toString());
+            final CustomTabsFallback fallback = factory.createFallback(options);
+            CustomTabsLauncher.launch(activity, customTabsIntent, uri, fallback);
+            result.success(null);
+        } catch (ActivityNotFoundException e) {
+            result.error(CODE_LAUNCH_ERROR, e.getMessage(), null);
+        }
+    }
 }
