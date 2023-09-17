1c23cc6e6cede0075148683de1e989727bdc593e
Shinya Kumagai <roomful.rooms@gmail.com>
2023-09-17 21:46:21 +0900

    Add `flutter_custom_tabs_android` to `flutter_custom_tabs` as an endorsed federated plugin

--- flutter_custom_tabs/lib/flutter_custom_tabs.dart
+++ flutter_custom_tabs/lib/flutter_custom_tabs.dart
@@ -1,7 +1,6 @@
 export 'package:flutter_custom_tabs_platform_interface/flutter_custom_tabs_platform_interface.dart'
     hide CustomTabsPlatform;
+export 'package:flutter_custom_tabs_android/flutter_custom_tabs_android.dart'
+    hide CustomTabsPlugin;
 
-export 'src/custom_tabs_animation.dart';
-export 'src/custom_tabs_close_button.dart';
-export 'src/custom_tabs_color_scheme.dart';
 export 'src/launcher.dart';
--- flutter_custom_tabs/lib/src/custom_tabs_close_button.dart
+++
@@ -1,5 +0,0 @@
-/// Build-in close button icon on custom tab.
-class CustomTabsCloseButtonIcon {
-  /// Gets the resource identifier of build-in back button icon.
-  static String get back => "fct_ic_arrow_back";
-}
