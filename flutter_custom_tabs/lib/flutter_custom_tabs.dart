aff7ddcec5a936c8793a4d5a1d0cd4ce8a31afe0
Shinya Kumagai <roomful.rooms@gmail.com>
2023-09-18 13:54:25 +0900

    Fix warnings generated by Flutter's static analysis

--- flutter_custom_tabs/lib/flutter_custom_tabs.dart
+++ flutter_custom_tabs/lib/flutter_custom_tabs.dart
@@ -1,6 +1,6 @@
 export 'package:flutter_custom_tabs_platform_interface/flutter_custom_tabs_platform_interface.dart'
     hide CustomTabsPlatform;
 export 'package:flutter_custom_tabs_android/flutter_custom_tabs_android.dart'
-    hide CustomTabsPlugin;
+    hide CustomTabsPluginAndroid;
 
 export 'src/launcher.dart';
