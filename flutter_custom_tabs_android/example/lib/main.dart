58caa3b8b5805795278c761c481ec0deb51f4cbc
Shinya Kumagai <roomful.rooms@gmail.com>
2023-11-18 23:29:38 +0900

    Refactor `flutter_custom_tabs_android` example app

--- flutter_custom_tabs_android/example/lib/main.dart
+++ flutter_custom_tabs_android/example/lib/main.dart
@@ -29,146 +29,126 @@ class MyApp extends StatelessWidget {
             appBar: AppBar(
               title: const Text('Example for Android'),
             ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  FilledButton.tonal(
-                    onPressed: () => _launchURL(context),
-                    child: const Text(
-                      'Show flutter.dev',
-                    ),
-                  ),
-                  const SizedBox(height: 24),
-                  TextButton(
-                    onPressed: () => _launchURLInBottomSheet(context),
-                    child: Text(
-                      'Show flutter.dev in bottom sheet',
-                      style: TextStyle(
-                        fontSize:
-                            Theme.of(context).textTheme.bodyLarge?.fontSize,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  TextButton(
-                    onPressed: () => _launchDeepLinkURL(context),
-                    child: Text(
-                      'Deep linking to Google Maps',
-                      style: TextStyle(
-                        fontSize:
-                            Theme.of(context).textTheme.bodyLarge?.fontSize,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            body: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              children: <Widget>[
+                FilledButton.tonal(
+                  onPressed: () => _launchURL(context),
+                  child: const Text('Show flutter.dev'),
+                ),
+                FilledButton.tonal(
+                  onPressed: () => _launchURLInBottomSheet(context),
+                  child: const Text('Show flutter.dev in bottom sheet'),
+                ),
+                FilledButton.tonal(
+                  onPressed: () => _launchDeepLinkingURL(context),
+                  child: const Text('Deep link to Google Maps'),
+                ),
+              ],
             ),
           ),
         ),
       ),
     );
   }
+}
 
-  Future<void> _launchURL(BuildContext context) async {
-    final theme = Theme.of(context);
-    try {
-      await CustomTabsPlatform.instance.launch(
-        'https://flutter.dev',
-        customTabsOptions: CustomTabsOptions(
-          colorSchemes: CustomTabsColorSchemes.defaults(
-            toolbarColor: theme.colorScheme.surface,
-            navigationBarColor: theme.colorScheme.background,
-          ),
-          shareState: CustomTabsShareState.on,
-          urlBarHidingEnabled: true,
-          showTitle: true,
-          animations: CustomTabsSystemAnimations.slideIn(),
-          closeButton: CustomTabsCloseButton(
-            icon: CustomTabsCloseButtonIcon.back,
-          ),
+Future<void> _launchURL(BuildContext context) async {
+  final theme = Theme.of(context);
+  try {
+    await CustomTabsPlatform.instance.launch(
+      'https://flutter.dev',
+      customTabsOptions: CustomTabsOptions(
+        colorSchemes: CustomTabsColorSchemes.defaults(
+          toolbarColor: theme.colorScheme.surface,
+          navigationBarColor: theme.colorScheme.background,
         ),
-        safariVCOptions: SafariViewControllerOptions(
-          preferredBarTintColor: theme.colorScheme.surface,
-          preferredControlTintColor: theme.colorScheme.onSurface,
-          barCollapsingEnabled: true,
-          entersReaderIfAvailable: false,
-          dismissButtonStyle: SafariViewControllerDismissButtonStyle.close,
+        shareState: CustomTabsShareState.on,
+        urlBarHidingEnabled: true,
+        showTitle: true,
+        animations: CustomTabsSystemAnimations.slideIn(),
+        closeButton: CustomTabsCloseButton(
+          icon: CustomTabsCloseButtonIcon.back,
         ),
-      );
-    } catch (e) {
-      // An exception is thrown if browser app is not installed on Android device.
-      debugPrint(e.toString());
-    }
+      ),
+      safariVCOptions: SafariViewControllerOptions(
+        preferredBarTintColor: theme.colorScheme.surface,
+        preferredControlTintColor: theme.colorScheme.onSurface,
+        barCollapsingEnabled: true,
+        entersReaderIfAvailable: false,
+        dismissButtonStyle: SafariViewControllerDismissButtonStyle.close,
+      ),
+    );
+  } catch (e) {
+    // An exception is thrown if browser app is not installed on Android device.
+    debugPrint(e.toString());
   }
+}
 
-  Future<void> _launchURLInBottomSheet(BuildContext context) async {
-    final theme = Theme.of(context);
-    final mediaQuery = MediaQuery.of(context);
-    try {
-      await CustomTabsPlatform.instance.launch(
-        'https://flutter.dev',
-        customTabsOptions: CustomTabsOptions.partial(
-            configuration: PartialCustomTabsConfiguration(
-              initialHeight: mediaQuery.size.height * 0.7,
-            ),
-            colorSchemes: CustomTabsColorSchemes.defaults(
-              colorScheme: theme.brightness.toColorScheme(),
-              toolbarColor: theme.primaryColor,
-            ),
-            showTitle: true,
-            closeButton: const CustomTabsCloseButton(icon: "ic_round_close")),
-        safariVCOptions: SafariViewControllerOptions.pageSheet(
-          configuration: const SheetPresentationControllerConfiguration(
-            detents: {
-              SheetPresentationControllerDetent.large,
+Future<void> _launchURLInBottomSheet(BuildContext context) async {
+  final theme = Theme.of(context);
+  final mediaQuery = MediaQuery.of(context);
+  try {
+    await CustomTabsPlatform.instance.launch(
+      'https://flutter.dev',
+      customTabsOptions: CustomTabsOptions.partial(
+          configuration: PartialCustomTabsConfiguration(
+            initialHeight: mediaQuery.size.height * 0.7,
+          ),
+          colorSchemes: CustomTabsColorSchemes.defaults(
+            colorScheme: theme.brightness.toColorScheme(),
+            toolbarColor: theme.primaryColor,
+          ),
+          showTitle: true,
+          closeButton: const CustomTabsCloseButton(icon: "ic_round_close")),
+      safariVCOptions: SafariViewControllerOptions.pageSheet(
+        configuration: const SheetPresentationControllerConfiguration(
+          detents: {
+            SheetPresentationControllerDetent.large,
+            SheetPresentationControllerDetent.medium,
+          },
+          largestUndimmedDetentIdentifier:
               SheetPresentationControllerDetent.medium,
-            },
-            largestUndimmedDetentIdentifier:
-                SheetPresentationControllerDetent.medium,
-            prefersScrollingExpandsWhenScrolledToEdge: false,
-            prefersGrabberVisible: true,
-            prefersEdgeAttachedInCompactHeight: true,
-            preferredCornerRadius: 16.0,
-          ),
-          preferredBarTintColor: theme.primaryColor,
-          preferredControlTintColor: Colors.white,
-          entersReaderIfAvailable: true,
-          dismissButtonStyle: SafariViewControllerDismissButtonStyle.close,
+          prefersScrollingExpandsWhenScrolledToEdge: false,
+          prefersGrabberVisible: true,
+          prefersEdgeAttachedInCompactHeight: true,
+          preferredCornerRadius: 16.0,
         ),
-      );
-    } catch (e) {
-      // An exception is thrown if browser app is not installed on Android device.
-      debugPrint(e.toString());
-    }
+        preferredBarTintColor: theme.primaryColor,
+        preferredControlTintColor: Colors.white,
+        entersReaderIfAvailable: true,
+        dismissButtonStyle: SafariViewControllerDismissButtonStyle.close,
+      ),
+    );
+  } catch (e) {
+    debugPrint(e.toString());
   }
+}
 
-  Future<void> _launchDeepLinkURL(BuildContext context) async {
-    final theme = Theme.of(context);
-    try {
-      await CustomTabsPlatform.instance.launch(
-        'https://www.google.co.jp/maps/@35.6908883,139.7865242,13.53z',
-        prefersDeepLink: true,
-        customTabsOptions: CustomTabsOptions(
-          colorSchemes: CustomTabsColorSchemes.defaults(
-            toolbarColor: theme.colorScheme.surface,
-            navigationBarColor: theme.colorScheme.background,
-          ),
-          shareState: CustomTabsShareState.on,
-          urlBarHidingEnabled: true,
-          showTitle: true,
+Future<void> _launchDeepLinkingURL(BuildContext context) async {
+  final theme = Theme.of(context);
+  try {
+    await CustomTabsPlatform.instance.launch(
+      'https://www.google.co.jp/maps/@35.6908883,139.7865242,13.53z',
+      prefersDeepLink: true,
+      customTabsOptions: CustomTabsOptions(
+        colorSchemes: CustomTabsColorSchemes.defaults(
+          toolbarColor: theme.colorScheme.surface,
+          navigationBarColor: theme.colorScheme.background,
         ),
-        safariVCOptions: SafariViewControllerOptions(
-          preferredBarTintColor: theme.colorScheme.surface,
-          preferredControlTintColor: theme.colorScheme.onSurface,
-          barCollapsingEnabled: true,
-          entersReaderIfAvailable: false,
-          dismissButtonStyle: SafariViewControllerDismissButtonStyle.close,
-        ),
-      );
-    } catch (e) {
-      // An exception is thrown if browser app is not installed on Android device.
-      debugPrint(e.toString());
-    }
+        shareState: CustomTabsShareState.on,
+        urlBarHidingEnabled: true,
+        showTitle: true,
+      ),
+      safariVCOptions: SafariViewControllerOptions(
+        preferredBarTintColor: theme.colorScheme.surface,
+        preferredControlTintColor: theme.colorScheme.onSurface,
+        barCollapsingEnabled: true,
+        entersReaderIfAvailable: false,
+        dismissButtonStyle: SafariViewControllerDismissButtonStyle.close,
+      ),
+    );
+  } catch (e) {
+    debugPrint(e.toString());
   }
 }
