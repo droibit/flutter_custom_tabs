423cff5e1766144a684198fd0c8a29f8924c38b9
Shinya Kumagai <roomful.rooms@gmail.com>
2025-01-11 21:09:42 +0900

    Refactor constants into companion objects

--- flutter_custom_tabs_android/android/src/main/kotlin/com/github/droibit/flutter/plugins/customtabs/core/utils/Utils.kt
+++ flutter_custom_tabs_android/android/src/main/kotlin/com/github/droibit/flutter/plugins/customtabs/core/utils/Utils.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 
 internal const val TAG = "CustomTabsAndroid"
 
-internal const val CODE_LAUNCH_ERROR: String = "LAUNCH_ERROR"
-internal const val REQUEST_CODE_PARTIAL_CUSTOM_TABS = 1001
-
 internal fun extractBundle(headers: Map<String, String>): Bundle {
     return Bundle(headers.size).apply {
         for ((key, value) in headers) {
